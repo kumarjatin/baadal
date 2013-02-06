3bae85e6a65de3ab39774ec93c5c509c1bc51da4
Saransh Gupta <saransh.jiit@gmail.com>
2013-02-06 13:25:54 +0530

    update

--- host/install_host.sh
+++ host/install_host.sh
@@ -1,45 +1,85 @@
-apt-get -y update
-apt-get -y dist-upgrade
-path=/root/
-echo "Configure networking"
-echo "Setting up network configuration on the host.
-Note : This will override your present network configuration. But you can backup the network anytime from the backup file created.
-==========================================================="
+Normal_pkg_lst=(python kvm qemu qemu-kvm libvirt-bin libvirt0 python-libvirt gettext python-urlgrabber python-gtk-vnc virtinst nfs-common virt-top kvm-ipxe vlan munin-node munin-libvirt-plugins)
 
-echo "Installing some useful packages"
-echo "==============================="
-apt-get install -y python
-apt-get install -y kvm
-apt-get install -y qemu
-apt-get install -y qemu-kvm
-#apt-get install -y virt-manager
-apt-get install -y libvirt-bin
-apt-get install -y libvirt0
-apt-get install -y python-libvirt
-apt-get install -y gettext
-apt-get install -y python-urlgrabber
-apt-get install -y python-gtk-vnc
-apt-get install -y virtinst
-apt-get install -y nfs-common
-apt-get install -y virt-top
-apt-get install -y kvm-pxe
-apt-get install -y vlan
-apt-get install -y munin-node
-apt-get install -y munin-libvirt-plugins
+Chk_Root_Login()
+{
+	username=`whoami`
+	if test $username != "root"; then
+
+  		echo "LOGIN AS SUPER USER(root) TO INSTALL BAADAL!!!"
+  		exit 1
+	fi
+
+	echo "User Logged in as Root............................................"
+}
+
+
+#Function that install all the packages packages
+Instl_Pkgs()
+{	
+	echo "Installing some useful packages"
+	echo "==============================="
+
+	Pkg_lst=()
+	Pkg_lst=${Normal_pkg_lst[@]}
+
+	for pkg_multi_vrsn in ${Pkg_lst[@]}; do
+
+		pkg_status=0
+		pkg_multi_vrsn=(`echo $pkg_multi_vrsn | tr ":" " "`)
+ 		
+		for pkg in ${pkg_multi_vrsn[@]}; do
+
+			dpkg-query -S $pkg>/dev/null;
+	  		status=$?;
+ 	
+			if test $status -eq 1;  then 
+
+		        	echo "$pkg Package not installed................"
+				echo "Installing Package: $pkg.................."
+				apt-get -y install $pkg --force-yes
+				status=$?
+
+				if test $status -eq 0 ; then 
+		      
+					echo "$pkg Package Installed Successfully" 
+					pkg_status=1
+					break
+			 	fi
+
+		        elif test $status -eq 0 ; then
+
+		        	echo "$pkg Package Already Installed" 
+				pkg_status=1;break     
+			fi
+		done
+		
+		if test $pkg_status -eq 0; then
+			
+			echo "PACKAGE INSTALLATION UNSUCCESSFULL: ${pkg_multi_vrsn[@]} !!!"
+			echo "NETWORK CONNECTION ERROR/ REPOSITORY ERROR!!!"
+			exit 1 
+		fi	
+	done
+
+	echo "Packages Installed Successfully..................................."
+}
+
+
+Enbl_Modules()
+{
 
 echo "Enabling KVM support"
 echo "===================="
+
 modprobe kvm
 modprobe kvm_intel
 
-#lsmod | grep kvm
-#/etc/init.d/libvirtd start
 mkdir /vms
 mkdir /vms/ds_kvm1
 mkdir /vms/ds_kvm2
+
 #Assuming your host is already in our export list
 size=`cat /etc/fstab |wc -l`|head -n $(($size-2)) /etc/fstab > /etc/fstab
-
 
 echo "Editing /etc/libvirt/qemu.conf to set\n user=\"root\"\n group=\"root\. Bkup file created in local folder\n"
 cp /lib/libvirt/qemu.conf  bkup-qemu.conf
@@ -54,8 +94,12 @@ echo "Replacing /etc/default/nfs-common with ideal-nfs-common. Bkup in bkup-nfs-
 cp /etc/default/nfs-common bkup-nfs-common
 cp ideal-nfs-common /etc/default/nfs-common
 
-
 echo "Configuring munin"
 echo "If you have done all the steps correctly, Congos!!! you are done with adding the host"
-#echo "Restarting the system"
-#shutdown -r now
+}
+
+#Script
+
+Chk_Root_Login
+Instl_Pkgs
+Enbl_Modules
